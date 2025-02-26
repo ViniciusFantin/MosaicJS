ebbf2ea6e9643606b13c03c252e556a0b5c25f2e
Vinicius Fantin <vinifantin302@gmail.com>
2025-02-26 18:14:10 -0300

    feat - css/styles

--- mosaic/src/pages/Register/Register.js
+++ mosaic/src/pages/Register/Register.js
@@ -1,9 +1,40 @@
 import styles from "./Register.module.css";
 
+import { useEffect, useState } from "react";
+
 const Register = () => {
   return (
     <div>
-      <h1>Register</h1>
+      <h1>register to post</h1>
+      <p>Create your username and share your memories and stories!</p>
+      <form>
+        <label>
+          <span>Username</span>
+          <input
+            type="text"
+            name="displayName"
+            required
+            placeholder="Your Username"
+          />
+          <span>E-mail</span>
+          <input type="email" name="email" required placeholder="Your E-mail" />
+          <span>Password</span>
+          <input
+            type="password"
+            name="password"
+            required
+            placeholder="Password"
+          />
+          <span>Confirm Password</span>
+          <input
+            type="password"
+            name="confirmPassword"
+            required
+            placeholder="Confirm your password"
+          />
+        </label>
+        <button className="btn">Register</button>
+      </form>
     </div>
   );
 };
