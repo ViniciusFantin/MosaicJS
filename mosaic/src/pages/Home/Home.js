ec1573462c9d3d3ddf2ab6a1868eb4aa376b52ce
Vinicius Fantin <vinifantin302@gmail.com>
2025-03-07 17:36:30 -0300

    new - posts

--- mosaic/src/pages/Home/Home.js
+++ mosaic/src/pages/Home/Home.js
@@ -3,15 +3,17 @@ import styles from "./Home.module.css";
 
 // hooks
 import { useNavigate, Link } from "react-router-dom";
+import { useFetchDocument } from "../../hooks/useFetchDocument";
 
 // React imports
 import { useState } from "react";
 
 //components
+import PostDetail from "../../components/PostDetail";
 
 const Home = () => {
   const [query, setQuery] = useState("");
-  const [posts] = useState([]);
+  const { documents: posts, loading } = useFetchDocument("posts");
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -24,12 +26,13 @@ const Home = () => {
         <input
           type="text"
           placeholder="Or shearch for a tag..."
-          onChange={(e) => setQuery(e.target.values)}
+          onChange={(e) => setQuery(e.target.value)}
         />
         <button className="btn btn-dark">Search</button>
       </form>
       <div className="post-list">
-        <h1>Posts...</h1>
+        {loading && <p>Loading...</p>}
+        {posts && posts.map((post) => <PostDetail key={post.id} post={post} />)}
         {posts && posts.length === 0 && (
           <div className={styles.noposts}>
             <p>No post avaliable</p>
